df8e71cfe335c5cab5c1b1d18b8bf4aa9f1f1661
Jan Hancic <jan.hancic@gmail.com>
2012-12-23 11:41:18 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sdszombi::Application.config.secret_token = 'fb0a4a7f5da033f7d2c87e86439a3f927461040934b9bbd50b8acfe033eed3f6dee4bf44d2498e922da196c2c7d43f25ff0f81020b001a764a81c76cdbc30e3c'
