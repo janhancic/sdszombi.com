7f593ac6041d8a15705f80eb311ad15aeaa2da3d
Jan Hancic <jan.hancic@gmail.com>
2012-12-25 16:13:50 +0100

    move zombie count part to application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery
+	before_filter( :provide_zombie_count )
+
+	def provide_zombie_count
+		@all_zombies_num = Zombie.count()
+	end
 end
